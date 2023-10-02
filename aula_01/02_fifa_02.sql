54601b6f85748bc51b0bfb5ae33ec0c917d21a89
fggabardo <97553082+fggabardo@users.noreply.github.com>
2023-10-02 16:56:29 -0300

    Create 02_fifa_02.sql
    Realize uma consulta que retorne o nome e apenas os campos que permitem valores numéricos com casas decimais dos
    jogadores da tabela fifa. Quantos campos temos nessa condição?

---
+++ aula_01/02_fifa_02.sql
@@ -0,0 +1,13 @@
+### Realize uma consulta que retorne o nome e apenas os campos que permitem valores numéricos com casas decimais dos jogadores da tabela fifa. Quantos campos temos nessa condição?
+
+SELECT 
+
+	ARRANCADA
+    ,CAMISA
+    ,CONTROLE_DE_BOLA
+    ,ENERGIA
+    ,EQUILÍBRIO
+    ,FORCA
+    ,REPUTACAO_INTERNACIONAL
+    
+FROM fifa
