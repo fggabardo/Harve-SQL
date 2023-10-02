c65cf8c83f74011f0956f222052dc82aab8cc22d
fggabardo <97553082+fggabardo@users.noreply.github.com>
2023-10-02 19:52:40 -0300

    Create 01_fifa_01.sql
     Quantos jogadores temos com mais de 38 anos e que possuem mais de 80 na pontuação geral?

---
+++ aula_02/01_fifa_01.sql
@@ -0,0 +1,11 @@
+###  Quantos jogadores temos com mais de 38 anos e que possuem mais de 80 na pontuação geral?
+
+SELECT 
+
+	*
+
+FROM fifa
+
+WHERE IDADE > 38 AND PONTUACAO_GERAL > 80
+
+### Resultado: G. Buffon, 
