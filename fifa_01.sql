2b060cc5a24e11358411caa77d5a0a089e87fd6a
fggabardo <97553082+fggabardo@users.noreply.github.com>
2023-10-02 16:48:49 -0300

    Exercise 1: fifa_01.sql
    Realize uma consulta que retorne o nome e time de todos os jogadores da tabela fifa. Quantos jogadores temos nessa
    tabela?

---
+++ fifa_01.sql
@@ -0,0 +1,10 @@
+### Realize uma consulta que retorne o nome e time de todos os jogadores da tabela fifa. Quantos jogadores temos nessa tabela?
+
+SELECT 
+
+	NOME
+    ,TIME
+    
+FROM fifa
+
+### Resultado: 18208 registros
