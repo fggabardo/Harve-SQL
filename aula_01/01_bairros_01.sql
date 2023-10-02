513c7eede8491dc98c72455576f2e7018b0b53f0
fggabardo <97553082+fggabardo@users.noreply.github.com>
2023-10-02 16:59:00 -0300

    Create 01_bairros_01.sql
    Realize uma consulta que retorno o nome e apenas os campos numéricos (int ou float) da tabela Bairros. Quantos
    campos temos nessa condição?

---
+++ aula_01/01_bairros_01.sql
@@ -0,0 +1,8 @@
+### Realize uma consulta que retorno o nome e apenas os campos numéricos (int ou float) da tabela Bairros. Quantos campos temos nessa condição?
+
+SELECT 
+
+	area
+  ,populacao
+    
+FROM bairros
