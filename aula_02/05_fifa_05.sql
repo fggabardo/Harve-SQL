3d0fb57e04141324d16e5dd9c3399f612ab82587
fggabardo <97553082+fggabardo@users.noreply.github.com>
2023-10-02 22:21:47 -0300

    Create 05_fifa_05.sql
    * Realize uma consulta do nome e pontuação dos jogadores, mas renomeie estes campos com outros nomes(sugestão: para o inglês)

---
+++ aula_02/05_fifa_05.sql
@@ -0,0 +1,9 @@
+
+###  * Realize uma consulta do nome e pontuação dos jogadores, mas renomeie estes campos com outros nomes(sugestão: para o inglês)
+
+SELECT 
+
+	NOME AS nickname
+    ,PONTUACAO_GERAL AS score
+
+FROM fifa
