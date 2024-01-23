3030774fd004053fc9e8949406f954b3390bf2c6
fggabardo <97553082+fggabardo@users.noreply.github.com>
2024-01-23 13:41:54 -0300

    Update 02_fifa_02.sql

--- aula_02/02_fifa_02.sql
+++ aula_02/02_fifa_02.sql
@@ -1,10 +1,5 @@
 ###  * Quantos jogadores temos da itália que jogam em um dos principais times de lá, juventus ou milan?
 ### * Quantos jogadores possuem energia a partir de 60, idade maior que 30 anos e que não jogam nas posições de atacante e zagueiro.
 
-SELECT 
-
-	*
-
-FROM fifa
-
-WHERE IDNACIONALIDADE = 80 AND (TIME = 'juventus' OR TIME = 'milan')
+SELECT NOME FROM `fifa`
+WHERE TIME = 'juventus' OR TIME = 'milan'
